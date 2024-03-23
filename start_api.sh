42a19a2affa3c5ec420e176503125baba3650862
Wesley00s <wesley300rodrigues@gmail.com>
2024-03-23 20:34:54 -0300

    Commit

--- start_api.sh
+++ start_api.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./mvnw spring-boot:run -Dspring-boot.run.profiles=dev -Dspring-boot.run.jvmArguments="-Xmx256m -Xms128m" -Dspring-boot.run.arguments="'--DB_USER=postgres' '--DB_PASSWORD=123456' '--DB_NAME=crud_db'"
+./gradlew bootRun -Dspring-boot.run.profiles=dev -Dspring-boot.run.jvmArguments="-Xmx256m -Xms128m" -Dspring-boot.run.arguments="'--DB_USER=postgres' '--DB_PASSWORD=123456' '--DB_NAME=crud_db'"
